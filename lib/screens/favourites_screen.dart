b6ded384de95fddc541aa95444dd131fe954eba6
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-02-11 22:03:58 +0600

    completed favorites screen to show favorite items

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:food_app/models/meal.dart';
+import 'package:food_app/widgets/meal_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
+  final List<Meal> favoritesMeals;
+
+  FavoritesScreen(this.favoritesMeals);
+
+  @override
+  _FavoritesScreenState createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  void _removeMeal(String mealId) {
+    setState(() {
+      widget.favoritesMeals.removeWhere((element) => element.id == mealId);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The Favorites'),
-    );
+    if (widget.favoritesMeals.isEmpty) {
+      return Center(
+        child: Text('The Favorites'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: widget.favoritesMeals[index].id,
+            title: widget.favoritesMeals[index].title,
+            imageUrl: widget.favoritesMeals[index].imageUrl,
+            duration: widget.favoritesMeals[index].duration,
+            complexity: widget.favoritesMeals[index].complexity,
+            affordability: widget.favoritesMeals[index].affordability,
+            removeItem: _removeMeal,
+          );
+        },
+        itemCount: widget.favoritesMeals.length,
+      );
+    }
   }
 }
