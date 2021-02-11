3021069ac6f2dc3bb13ca11c47caf70fbf912cb0
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-02-11 22:02:16 +0600

    Added routing , global variables for different page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/dummy_data.dart';
+import 'package:food_app/models/meal.dart';
+import 'package:food_app/screens/filer_screen.dart';
 import 'package:food_app/screens/meal_detail_screen.dart';
 import 'package:food_app/screens/tab_screen_bottom.dart';
 
-import 'file:///T:/WorkSpace/Developing/Flutter/food_app/lib/screens/category_meals_screen.dart';
-
+import './screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeal = DUMMY_MEALS;
+  List<Meal> _favoritesMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeal = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+
+        return true;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoritesMeals.indexWhere((element) => element.id == mealId);
+
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoritesMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoritesMeals
+            .add(DUMMY_MEALS.firstWhere((element) => element.id == mealId));
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoritesMeals.any((element) => element.id == id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,9 +102,15 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => BottomTabScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        '/': (ctx) => BottomTabScreen(_favoritesMeals),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeal),
+        MealDetailScreen.routeName: (ctx) =>
+            MealDetailScreen(_toggleFavorite, _isMealFavorite),
+        FilersScreen.routeName: (ctx) => FilersScreen(
+              saveFilters: _setFilters,
+              currentFilters: _filters,
+            ),
       },
 
       /// On Generate Route needed if some unexpected routing happens in
